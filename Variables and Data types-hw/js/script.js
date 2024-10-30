99cf9462a23b6e352d118902b415c882e5f068b8
Aziz <baziz1275@gmail.com>
2024-10-31 01:28:26 +0600

    home work Variables and Data types

---
+++ Variables and Data types-hw/js/script.js
@@ -0,0 +1,12 @@
+var name = "Aziz"
+var age = 18
+let isStudent = true;
+
+
+
+console.log("Аты:" + name + "/" + typeof name );
+console.log("Жасы:" + age + "/" + typeof age);
+console.log("Студент пе?:" + isStudent + "/" + typeof isStudent);
+console.log("Жасы" + "(жол ретінде:)" + age + typeof toString(age))
+
+
