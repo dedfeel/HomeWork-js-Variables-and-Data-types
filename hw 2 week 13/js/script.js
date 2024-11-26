df9a395c45590c1e947e169ffba74e78471d881c
Aziz <baziz1275@gmail.com>
2024-11-27 00:35:27 +0600

    hw 2 week 13

---
+++ hw 2 week 13/js/script.js
@@ -0,0 +1,31 @@
+let oinshylar = ["Роналду", "Зидан", "Неймар", "Бензема."]
+oinshylar[1] = "Messi"
+oinshylar[4] = "Ramos"
+oinshylar[5] = "Aziz"
+console.log(oinshylar);
+if(oinshylar.length > 5){
+    console.log("Oiynshylar sany 5 ten kop");
+}else if( oinshylar.length < 5){
+    console.log("Oiynshylar sany 5 ten az");
+}else if (oinshylar.length = 5) {
+    console.log("Oiynshylar sany 5 ke ten");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// 1.Төрт футболисттен тұратын массив ашылу керек. Футболисстер мына ретпен сақталуы тиіс: Роналду, Зидан, Неймар, Бензема.
+
+// 2.Зидан зейнетке шығып кетгендіктен оның орнына "Месси" деген мәнмен ауыстырамыз.
+
+// 3.Және массивтің соңына бірнеше футболисстерді қосасыздар.
+
+// 4. Егер футболист саны 5 еуден көп болса "көп" деп шығару керек, аз болса "аз" деп шығару керек
